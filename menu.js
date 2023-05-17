3a8f54a3c2e71e7cfdaa52a8f288e812f50e9d58
CoffeeAddicts <104673207+CoffeeAddicts@users.noreply.github.com>
2023-05-17 22:55:15 +0100

    Changes

--- menu.js
+++ menu.js
@@ -1,8 +1,17 @@
 const menu = document.getElementById("menu");
 
+
+
 Array.from(document.getElementsByClassName("menu-item"))
   .forEach((item, index) => {
     item.onmouseover = () => {
       menu.dataset.activeIndex = index;
     }
   });
+
+
+// function myFunction(){
+//   const test1 = document.getElementById("test1");
+//   test1.remove();
+// }
+
